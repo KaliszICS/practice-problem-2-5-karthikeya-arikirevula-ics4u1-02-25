1cb89acee8fd94a0d635b2a92e58716929a933fe
Karthikeya Arikirevula <karthikeya.arikirevula@student.tdsb.on.ca>
2025-04-03 22:58:57 +0000

    a

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -3,6 +3,17 @@ public class PracticeProblem {
 	public static void main(String args[]) {
 
 	}
+	public static void insertionSort(char[] arr) {
+		for(int i = 1; i < arr.length; i++) {
+			char k = arr[i];
+			int index = i - 1;
 
+			while(index >= 0 && arr[index] > k) {
+				arr[index + 1] = arr[index];
+				index--;
+			}
+			arr[index + 1] = k;
+		}
+	}
 
 }
